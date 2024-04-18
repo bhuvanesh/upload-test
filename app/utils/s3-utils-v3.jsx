299897858af70a049309d62ba258cdbcfe1f5f92
bhuvanesh <iambhuvanesh@gmail.com>
2024-04-18 18:29:48 +0530

    Tidy up the s3-utils-v3

--- app/utils/s3-utils-v3.jsx
+++ app/utils/s3-utils-v3.jsx
@@ -1,55 +1,13 @@
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import {
-    getSignedUrl,
-    S3RequestPresigner,
+    getSignedUrl
   } from "@aws-sdk/s3-request-presigner";
   import https from "https";
 
 
 const createPresignedUrlWithClient = ({ client, command}) => {
-    // const creds = {
-    //     accessKeyId: accessKeyId,
-    //     secretAccessKey: secretAccessKey,
-    // };
-    // const client = new S3Client({ region:region, credentials:creds});
-    // const command = new PutObjectCommand({ Bucket: bucket, Key: key });
     return getSignedUrl(client, command, { expiresIn: 3600 });
   };
-
-  async function put(url, data) {
-    // return new Promise((resolve, reject) => {
-    //   const req = https.request(
-    //     url,
-    //     { method: "PUT", headers: { "Content-Length": new Blob([data]).size } },
-    //     (res) => {
-    //       let responseBody = "";
-    //       res.on("data", (chunk) => {
-    //         responseBody += chunk;
-    //       });
-    //       res.on("end", () => {
-    //         resolve(responseBody);
-    //       });
-    //     },
-    //   );
-    //   req.on("error", (err) => {
-    //     reject(err);
-    //   });
-    //   req.write(data);
-    //   req.end();
-    // });
-    try{
-        const response = await fetch(url, {
-            method: 'PUT',
-            body: data,
-          });
-    }catch(e){
-        console.log(e)
-    }
-    
-
-  }
-  
-
 
   const uploadS3new = async (file, filePath, ENV)=>{
     const creds = {
@@ -66,19 +24,22 @@ const createPresignedUrlWithClient = ({ client, command}) => {
             method: 'PUT',
             body: await file.get("file"),
           });
-        // await put(presignUrl, await file.get("file"));
+
+    if (!response.ok) {
+        throw new Error('Failed to upload file to S3');
+      }
+      else{
+        console.log("Upload Success")
+      }
+
     }catch(err){
         console.log(err, response)
     }finally{
-        return presignUrl
+        const objectUrl = `https://${ENV.BUCKET_NAME}.s3.${ENV.REGION}.amazonaws.com/${ENV.fileName}`;
+        return objectUrl;
     }
     // return presignUrl
   }
 
-//   AWS.config.update({
-//     accessKeyId: ENV.ACCESS_KEY_ID,
-//     secretAccessKey: ENV.SECRET_ACCESS_KEY,
-//     region: ENV.REGION,
-//   });
 
-export {createPresignedUrlWithClient, uploadS3new}
+export {uploadS3new}
